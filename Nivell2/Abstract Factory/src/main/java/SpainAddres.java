816da57cbd4a26804028852995970b42692bf75a
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-11 12:32:47 +0100

    build

--- Nivell2/Abstract Factory/src/main/java/SpainAddres.java
+++ Nivell2/Abstract Factory/src/main/java/SpainAddres.java
@@ -1,7 +1,14 @@
 public class SpainAddres implements Address {
+    private String street;
+    private int doorNumber;
+
+    public SpainAddres(String street, ) {
+        this.street = street;
+    }
+
     @Override
     public String getAddress() {
-        return "";
+        return street + doorNumber + floor;
     }
 
 
--- Nivell2/Abstract Factory/src/main/java/SpainPhone.java
+++ Nivell2/Abstract Factory/src/main/java/SpainPhone.java
@@ -1,6 +1,13 @@
 public class SpainPhone implements PhoneNumber {
+    private String number;
+
+    public SpainPhone(String number) {
+        this.number = number;
+
+    }
+
     @Override
     public String getPhoneNumber() {
-        return "+34 " + phone;
+        return "+34 " + number;
     }
 }
