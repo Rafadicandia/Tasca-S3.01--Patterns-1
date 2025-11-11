affb4e4d4e155a51b29abbb41833aef691764651
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-11 10:38:25 +0100

    build

--- Nivell1/untitled/src/main/java/Undo.java
+++ Nivell1/untitled/src/main/java/Undo.java
@@ -1,8 +1,12 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Undo {
     private static Undo uniqueInstance;
+    private Deque<String> commandHistory;
 
     private Undo() {
-
+        this.commandHistory = new ArrayDeque<>();
     }
 
     public static Object getInstance() {
@@ -12,6 +16,27 @@ public class Undo {
         return Undo.uniqueInstance;
 
     }
+
+    public void addCommand(String command) {
+        this.commandHistory.addFirst(command);
+
+    }
+
+    public String undo(String command) {
+        if (this.commandHistory.isEmpty()) {
+            return "There are no commands to erase";
+        }
+
+        this.commandHistory.removeFirst();
+
+        return this.commandHistory.getFirst();
+    }
+
+    public <T> void listCommands() {
         
+        for (String t : this.commandHistory)
+            toString().indent(t);
+    }
+
 
 }
