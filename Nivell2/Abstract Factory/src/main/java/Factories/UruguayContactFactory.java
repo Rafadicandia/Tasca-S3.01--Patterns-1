3d21cdb6e934f31afec9741331b2ff94def4a095
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-12 10:43:27 +0100

    build

---
+++ Nivell2/Abstract Factory/src/main/java/Factories/UruguayContactFactory.java
@@ -0,0 +1,20 @@
+package Factories;
+
+import Interfaces.Address;
+import Interfaces.ContactFactory;
+import Interfaces.PhoneNumber;
+import Uruguay.UruguayAddress;
+import Uruguay.UruguayPhone;
+
+public class UruguayContactFactory implements ContactFactory {
+
+    @Override
+    public Address createAddress() {
+        return new UruguayAddress("Manuel Alonso", 1688, "10", "1", "11600", "Montevideo", "Uruguay");
+    }
+
+    @Override
+    public PhoneNumber createPhoneNumber() {
+        return new UruguayPhone("09898312416");
+    }
+}
