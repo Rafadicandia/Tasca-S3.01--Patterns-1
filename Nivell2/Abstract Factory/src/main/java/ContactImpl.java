b235fc7d57843a775d934b43cf54d9f16901d93a
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-13 10:54:37 +0100

    refactor

--- Nivell2/Abstract Factory/src/main/java/ContactImpl.java
+++
@@ -1,9 +0,0 @@
-import Interfaces.Address;
-import Interfaces.NameAndLastName;
-import Interfaces.PhoneNumber;
-
-public class ContactImpl extends Contact {
-    public ContactImpl(NameAndLastName nameDetails, Address address, PhoneNumber phone) {
-        super(nameDetails, address, phone);
-    }
-}
--- Nivell2/Abstract Factory/src/main/java/Factories/SpainContactFactory.java
+++ Nivell2/Abstract Factory/src/main/java/Factories/SpainContactFactory.java
@@ -8,17 +8,19 @@ import Spain.SpainPhone;
 public class SpainContactFactory implements ContactFactory {
 
     @Override
-    public Address createAddress(String street, int number, String door, String floor, String postalCode, String city, String region) {
-        return new SpainAddress(street, number, door, floor, postalCode, city, region);
+    public Address createAddress() {
+        return new SpainAddress("Carretera de Rubí", 135, "3", "2", "08223", "Terrassa", "Catalunya");
     }
 
     @Override
     public PhoneNumber createPhoneNumber() {
+
         return new SpainPhone("634330718");
     }
 
     @Override
     public NameAndLastName createFullName() {
+
         return new FullName("Rafael", "di Candia");
     }
 
