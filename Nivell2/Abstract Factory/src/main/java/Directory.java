c7083d44bbd4109a6e7ab380ae4e3cebee5b1ae6
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-13 12:27:37 +0100

    refactor

--- Nivell2/Abstract Factory/src/main/java/Directory.java
+++ Nivell2/Abstract Factory/src/main/java/Directory.java
@@ -1,11 +1,13 @@
+import Domain.Contact;
+
 import java.util.ArrayList;
 
 public class Directory {
-    private String name;
+
     private ArrayList<Contact> contactNamesAndAdddress;
 
     public Directory(String name){
-        this.name = name;
+        
         this.contactNamesAndAdddress = new ArrayList<>();
     }
 
