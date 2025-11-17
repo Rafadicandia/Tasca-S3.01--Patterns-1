0244cbd1776f4e00000bfa8bf5f1e2d300f7dcd0
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-17 11:10:47 +0100

    builds stop command

---
+++ Nivell3/comand/src/main/java/StopCommand.java
@@ -0,0 +1,12 @@
+public class StopCommand implements Command {
+    private Vehicle vehicleToStop;
+
+    public StopCommand(Vehicle vehicleToStop) {
+        this.vehicleToStop = vehicleToStop;
+    }
+
+    @Override
+    public void execute() {
+        this.vehicleToStop.stop();
+    }
+}
