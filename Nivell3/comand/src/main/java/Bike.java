61a3dabb1877934ad4ecf7a8a87524526cc2d94f
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-17 11:15:22 +0100

    builds child classes for vehicle with methods

---
+++ Nivell3/comand/src/main/java/Bike.java
@@ -0,0 +1,24 @@
+public class Bike extends Vehicle {
+
+    public Bike(String type) {
+        super(type);
+    }
+
+    @Override
+    public void accelerate() {
+        System.out.println("The bike is accelerating");
+
+    }
+
+    @Override
+    public void start() {
+        System.out.println("The bike is accelerating");
+
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("The bike is accelerating");
+
+    }
+}
---
+++ Nivell3/comand/src/main/java/Car.java
@@ -0,0 +1,21 @@
+public class Car extends Vehicle {
+
+    public Car(String type) {
+        super(type);
+    }
+
+    @Override
+    public void accelerate() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
