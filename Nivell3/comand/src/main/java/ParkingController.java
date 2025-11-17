66d42f460ffd2b3b48cc3575179b8023743debe2
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-11-17 11:43:56 +0100

    builds setter and methods to call

--- Nivell3/comand/src/main/java/ParkingController.java
+++ Nivell3/comand/src/main/java/ParkingController.java
@@ -1,2 +1,15 @@
+import Commands.Command;
+
 public class ParkingController {
+    private Command command;
+
+    public Command setCommand(Command command) {
+        this.command = command;
+    }
+
+
+    public void executeCommand() {
+        this.command.execute();
+
+    }
 }
